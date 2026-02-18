8e41a515be141e31bb0784a74a66cb912e5b71cc
abhijit-01 <abhijittripathy606@gmail.com>
2026-02-18 12:18:23 +0530

    messages API done

---
+++ src/app/api/check-username-unique/route.ts
@@ -0,0 +1,54 @@
+import dbConnect from "@/src/lib/dbConnect";
+import UserModel from "@/src/models/User";
+import {success, z} from "zod";
+import { usernameValidation } from "@/src/schemas/signUpSchema";
+
+const UsernameQuerySchema = z.object({
+    username: usernameValidation
+});
+
+export async function GET(request : Request){
+    await dbConnect();
+
+    try {
+         const { searchParams } = new URL(request.url);
+         const queryParam = {username : searchParams.get("username")};
+
+         //validate with zod
+
+         const result = UsernameQuerySchema.safeParse(queryParam);
+         console.log("Validation result:", result);
+
+         if(!result.success){
+             return Response.json({
+                 success: false,
+                 message: "Invalid username format.",
+                 errors: result.error.format().username?._errors || []
+             }, { status: 400 });
+         }
+        const {username} = result.data;
+        const existingVerifiedUser = await UserModel.findOne({username,isVerified: true});
+
+        if(existingVerifiedUser){
+            return Response.json({
+                success: false,
+                message: "Username is already taken."
+            } , { status: 409 });
+        }
+        return Response.json({
+            success: true,  
+            message: "Username is available."
+        } , { status: 200 });
+
+
+
+
+
+    } catch (error) {
+        console.error("Error checking username uniqueness:", error);
+        return Response.json({
+            success: false,
+            message: "An error occurred while checking username uniqueness."
+        } ,{ status: 500})
+    }
+}
---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,58 @@
+import dbConnect from "@/src/lib/dbConnect";
+import UserModel from "@/src/models/User";
+import { decode } from "punycode";
+
+
+export async function POST(request : Request){
+    await dbConnect();
+
+    try {
+        const {username , code} = await request.json();
+
+        const decodedUsername = decodeURIComponent(username);
+
+        const user = await UserModel.findOne({username : decodedUsername})
+        if(!user){
+            return Response.json({
+                success: false,
+                message: "User not found."
+            } , { status: 404 });
+        }
+
+        const isCodeValid = user.verifyCode === code;
+        const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+
+        if(isCodeValid && isCodeNotExpired){
+            user.isVerified = true;
+            await user.save();
+
+            return Response.json({
+                success: true,
+                message: "Code verified successfully."
+            } , { status: 200 });
+        }
+
+        else if(!isCodeNotExpired){
+            return Response.json({
+                success: false,
+                message: "Verification code has expired."
+            } , { status: 400 });
+        }
+        else{
+            return Response.json({
+                success: false,
+                message: "Invalid verification code."
+            } , { status: 400 });
+        }
+
+
+
+
+    } catch (error) {
+        console.error("Error verifying code:", error);
+        return Response.json({
+            success: false,
+            message: "An error occurred while verifying the code."
+        } , { status: 500 });
+    }
+}
