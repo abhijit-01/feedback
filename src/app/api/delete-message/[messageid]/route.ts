5ae5e9ab8c1c7cf0c75a792c3107e3f5580f2d9a
abhijit-01 <abhijittripathy606@gmail.com>
2026-02-21 21:15:46 +0530

    dlt route updated

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -2,20 +2,22 @@ import UserModel from "@/models/User";
 import { getServerSession } from "next-auth/next";
 import dbConnect from "@/lib/dbConnect";
 import { User } from "next-auth";
-import { Message } from "@/models/User";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { authOptions } from "../../auth/[...nextauth]/options";
 
 export async function DELETE(
-  request: Request,
-  { params }: { params: { messageid: string } },
+  request: NextRequest,
+  context: { params: Promise<{ messageid: string }> },
 ) {
-  const messageId = params.messageid;
+  const { messageid } = await context.params; // ✅ FIXED
+
   await dbConnect();
+
   const session = await getServerSession(authOptions);
-  const _user: User = session?.user;
+  const _user: User | undefined = session?.user as User;
+
   if (!session || !_user) {
-    return Response.json(
+    return NextResponse.json(
       { success: false, message: "Not authenticated" },
       { status: 401 },
     );
@@ -24,23 +26,24 @@ export async function DELETE(
   try {
     const updateResult = await UserModel.updateOne(
       { _id: _user._id },
-      { $pull: { messages: { _id: messageId } } },
+      { $pull: { messages: { _id: messageid } } },
     );
 
     if (updateResult.modifiedCount === 0) {
-      return Response.json(
+      return NextResponse.json(
         { message: "Message not found or already deleted", success: false },
         { status: 404 },
       );
     }
 
-    return Response.json(
+    return NextResponse.json(
       { message: "Message deleted", success: true },
       { status: 200 },
     );
   } catch (error) {
     console.error("Error deleting message:", error);
-    return Response.json(
+
+    return NextResponse.json(
       { message: "Error deleting message", success: false },
       { status: 500 },
     );
